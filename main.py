03ea5e229e6e0ab8e55bf3e4cb1664beae0b262f
T. Coetzee <tc611@tw301.eng.cam.ac.uk>
2021-10-12 14:29:02 +0100

    finish turret init

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-from turret import Turret
+#!/usr/bin/env python3
+from turret import *
+from time import sleep
 
-config = {}
-t = Turret(config)
+t = Turret()
+
+sleep(2.5)
+t.pan(180)
+sleep(2.5)
+t.aim_at(25, 10)
+sleep(2.5)
+#t.fire()
+t.release_brakes()
 
 # wait for touch input to start, and set stop time ...
--- turret.py
+++ turret.py
@@ -4,19 +4,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class Turret: 
-    def __init__(self, config):
+    def __init__(self):
         print("initialising")
         self.brick = NXTBrick()
         self.pan_motor = Motor(self.brick, PORT_A)
         self.tilt_motor = Motor(self.brick, PORT_B)
-        self.fire_motor = Motor(self.brick, PORT_C)
+        #self.fire_motor = Motor(self.brick, PORT_C)
+
+        # set the turret tilt to 0 degrees and hold it
+        self.tilt_motor.turn(27.5, 25, True, True, True, True)
+        self.tilt_motor.wait_for()
 
         self.pan_motor.reset_position()
         self.tilt_motor.reset_position()
-        self.fire_motor.reset_position()
+        #self.fire_motor.reset_position()
 
-        self.ultrasonic = Ultrasonic(self.brick, PORT_1)
-        self.touch = Touch(self.brick, PORT_2)
+        #self.ultrasonic = Ultrasonic(self.brick, PORT_1)
+        #self.touch = Touch(self.brick, PORT_2)
 
 
     def start(self):
@@ -26,26 +30,29 @@ class Turret:
         print("scanning")
 
     def aim_at(self, ax, az):
-        print(f'aiming: [{ax}, {az}]')
-        self.pan_motor.turn_to(az, 50, True, True, True, False)
-        self.tilt_motor.turn_to(ax, 50, True, False, True, True)
+        self.pan_motor.turn_to(az, 10, True, True, True, False)
         self.pan_motor.wait_for()
+        self.tilt_motor.turn_to(-ax, 10, True, False, True, True)
         self.tilt_motor.wait_for()
 
 
     def fire(self):
-        print("firing")
         self.fire_motor.turn(5, 100, False, False, False, False)
         self.fire_motor.wait_for()
 
     def pan(self, angle):
-        print("rotating turret (az): " + angle)
-        self.pan_motor.turn(angle, 50, True, True, True, False)
+        self.pan_motor.turn(angle, 10, True, True, True, False)
         self.pan_motor.wait_for()
 
-    def tilt(self, angle):
-        print("rotating turret (ax): " + angle)
-        self.tilt_motor.turn(angle, 50, True, True, True, False)
+    def tilt(self, angle, hold):
+        self.tilt_motor.turn(-angle, 25, True, True, True, hold)
         self.tilt_motor.wait_for()
+
+    def home(self):
+        self.pan_motor.turn_to(0, 10)
+        self.tilt_motor.turn_to(0, 10)
+
+    def release_brakes(self):
+        self.tilt_motor.idle()
 
 
